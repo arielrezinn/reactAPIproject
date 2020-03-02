77f0e7416906f092e07fbb75586c880ecf3e91c0
arielrezinn <arielrezin@gmail.com>
2020-03-01 20:06:31 -0600

    Updates

---
+++ precache-manifest.837a8e01584acf68cb9e064c893f6c94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb0d5e4057bb0b77845b0f2963615b4",
+    "url": "/reactAPIproject/index.html"
+  },
+  {
+    "revision": "51c4df00eeb9abf8d0ff",
+    "url": "/reactAPIproject/static/css/main.c84cb061.chunk.css"
+  },
+  {
+    "revision": "e28ff6321dee2d2ce1fa",
+    "url": "/reactAPIproject/static/js/2.bfb403cd.chunk.js"
+  },
+  {
+    "revision": "9fee77bf5fc4380e0aeb31e5f5891869",
+    "url": "/reactAPIproject/static/js/2.bfb403cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51c4df00eeb9abf8d0ff",
+    "url": "/reactAPIproject/static/js/main.a97af981.chunk.js"
+  },
+  {
+    "revision": "c8a87acd421753ae6958",
+    "url": "/reactAPIproject/static/js/runtime-main.b5996150.js"
+  }
+]);
